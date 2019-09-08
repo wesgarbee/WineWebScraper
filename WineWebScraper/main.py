031194941fd32cf1150860e1cadb7229696a6bf9
Wes Garbee <wgarbee@gmail.com>
2019-09-07 21:30:10 -0500

    moved dir creation lines outside loop

--- WineWebScraper/main.py
+++ WineWebScraper/main.py
@@ -15,38 +15,41 @@ try:
     conn = MongoClient()
     print("Connected successfully!")
 except Exception as e:
-    print("Could not connect to MongoDB", e)
+    print("Could not connect to MongoDB. Error: ", e)
 
 # Connect to db
 db = conn.database
+collection = db.cava_wine_list
 
+# Increments the page number to scrape
 page_number = 0
-# address = "https://www.wine.com/list/wine/red-wine/cabernet-sauvignon/7155-124-139"
 
 # Prompts user to enter the wine.com url to scrape
 address = input("Enter wine.com URL: ").strip()
 req = requests.head(address)
 code = req.status_code
 
+wine_type = address.split("/")[-2]
+# Path name where the images for this search will be stored
+dir_name = ("./images/" + wine_type.replace("-", "_").lower())
+
+# If that dir does not exist, creates it.
+if not os.path.isdir(dir_name):
+    os.mkdir(dir_name)
+
 while code is 200:
-    address = address + "/" + str(page_number)
-    wine_type = address.split("/")[-3]
+    page_to_pull = address + "/" + str(page_number)
     print(wine_type)
 
-    # Path name where the images for this search will be stored
-    dir_name = ("./images/" + wine_type.replace("-", "_").lower())
-
-    # If that dir does not exist, creates it.
-    if not os.path.isdir(dir_name):
-        os.mkdir(dir_name)
-
-    req = requests.get(address)
+    req = requests.get(page_to_pull)
 
     soup = bSoup(req.text, "html.parser")
     wines = soup.findAll("li", {"class": "prodItem"})
 
     for wine in wines:
         wine_text = wine.find("span", {"class": "prodItemInfo_name"}).text
+        wine_id = wine.find("meta")['content']
+        wine_vintage = wine_text[-4:]
         wine_varietal_text = wine.find("span", {"class": "prodItemInfo_varietal"}).text
         wine_origin = wine.find("span", {"class": "prodItemInfo_originText"}).text
         wine_img_element = wine.find('img')
@@ -55,18 +58,28 @@ while code is 200:
         urllib.request.urlretrieve(wine_img, wine_img_path)
 
         try:
-            print(wine_text)
-            print(wine_varietal_text)
-            print(wine_origin)
-            print(wine_img_path)
+            wine = {"name": wine_text[:-4].strip(),
+                    "wine_id": wine_id,
+                    "vintage": wine_vintage,
+                    "varietal": wine_varietal_text,
+                    "origin": wine_origin,
+                    "label_image": wine_img_path}
+            rec_id = collection.insert_one(wine)
+            print("Data inserted with record id", rec_id)
         except Exception as e:
             print("Error: ", e)
 
     page_number += 1
 
-    # Sets delay so as not to overload the server
+    # # Sets delay between 90 and 120 seconds so as not to overload the server
     seconds = random.randrange(90, 120)
     time.sleep(seconds)
+
+    print(seconds)
+
+    # Sets delay between 90 and 120 seconds so as not to overload the server
+    # seconds = random.randrange(1, 5)
+    # time.sleep(seconds)
 
     # Gets status code.
     code = requests.head(address).status_code
